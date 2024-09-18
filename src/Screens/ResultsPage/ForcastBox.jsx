bc1ea226162bc0e7f5a6c31e5d5254bd2c0d8033
jharville <153254589+jharville@users.noreply.github.com>
2024-09-18 12:03:23 -0500

    Styling button and cleaning up comments from henry

--- src/Screens/ResultsPage/ForcastBox.jsx
+++ src/Screens/ResultsPage/ForcastBox.jsx
@@ -4,8 +4,13 @@ import { TbTemperatureFahrenheit } from "react-icons/tb";
 import { useState, useRef } from "react";
 import { FormattedDate } from "../FormattedDate.jsx";
 import { LoadingIcon } from "../LoadingIcon.jsx";
-// I have logic numbered in this file just incase you want to read the logic in order. 
-// search 1H, 2H, 3H etc in this file to see them
+
+const daySpanOptions = {
+  7: 7,
+  14: 14,
+};
+
+const daySpanOptionsArr = Object.values(daySpanOptions);
 
 export const ForcastBox = ({
   WeatherIcon,
@@ -15,21 +20,19 @@ export const ForcastBox = ({
   isLoading,
 }) => {
   const scrollContainerRef = useRef(null);
-  const [selectedDaySpanOption, setSelectedDaySpanOption] = useState(daySpanOptions[7]);
+  const [selectedDaySpanOption, setSelectedDaySpanOption] = useState(
+    daySpanOptions[7]
+  );
 
-  // 3H
   const handleSelectOption = (selectedOption) => {
-    // since we're using the daySpanOptionsArr array, and it's values are numbers, we get to side-step
-    // all of the type conversion logic here. We're just working with numbers, simple.
-    // Since we simplified the logic & got rid of one of the useStates, we can just update the one state variable
-    // & it will drive everything else.
-    setSelectedDaySpanOption(selectedOption)
+    setSelectedDaySpanOption(selectedOption);
     if (scrollContainerRef.current) {
       scrollContainerRef.current.scrollTop = 0;
     }
   };
 
-  const forecastDaysClass = selectedDaySpanOption <= daySpanOptions[7] ? "hide-scrollbar" : "";
+  const forecastDaysClass =
+    selectedDaySpanOption <= daySpanOptions[7] ? "hide-scrollbar" : "";
 
   const isValidForecast = forcastDate?.length;
 
@@ -38,26 +41,21 @@ export const ForcastBox = ({
       <div id="contents-gap">
         <div id="forcast-text-and-day-selector-container">
           <p id="forcast-text">Forecast</p>
-           {isValidForecast && (
-             <div id="day-selector-container">
+          {isValidForecast && (
+            <div id="day-selector-container">
               {daySpanOptionsArr.map((option, index) => {
-              // 4H
-              // This is what I really like about .map() for options.
-              // In here we can use the option that gets passed in to decide everything.
-              // we also can get rid of the className variables that were in this file 
-              // since we can just compare each option individually to the selected option.
                 const isSelected = selectedDaySpanOption === option;
-                const label = option === 1 ? `${option} Day` : `${option} Day`
+                const label = option === 1 ? `${option} Day` : `${option} Day`;
                 return (
-              <button
-              key={index}
-                id="day-param-select"
-                className={isSelected && "clicked"}
-                onClick={() => handleSelectOption(option)}
-              >
-                <p>{label}</p>
-              </button>
-                )
+                  <button
+                    key={index}
+                    id="day-param-select"
+                    className={isSelected && "clicked"}
+                    onClick={() => handleSelectOption(option)}
+                  >
+                    <p>{label}</p>
+                  </button>
+                );
               })}
             </div>
           )}
@@ -75,18 +73,16 @@ export const ForcastBox = ({
             ) : isValidForecast ? (
               forcastDate
                 .slice(0, selectedDaySpanOption)
-                // 5H
-                // renamed this to i instead of dateID since it's just an index
-                .map((eachIndividualDate, i) => (
-                  <div key={i} id="clickable-forcast-day">
+                .map((eachIndividualDate, index) => (
+                  <div key={index} id="clickable-forcast-day">
                     <div id="icon-and-high-low-container">
                       <div id="weather-icon-styling">
-                        {getWeatherIcon(getWeatherLabel(WeatherIcon[i]))}
+                        {getWeatherIcon(getWeatherLabel(WeatherIcon[index]))}
                       </div>
                       <div id="temp-high-low-and-f-symbol">
                         <p id="temp-high-low-styling">
-                          {Math.round(minTemp[i])} /{" "}
-                          {Math.round(maxTemp[i])}
+                          {Math.round(minTemp[index])} /{" "}
+                          {Math.round(maxTemp[index])}
                         </p>
                         <div id="fahrenheit-symbol">
                           <TbTemperatureFahrenheit />
@@ -107,127 +103,3 @@ export const ForcastBox = ({
     </div>
   );
 };
-
-// 1H
-// daySpanOptions easily controls all the options for this component. If you want additional options,
-// you can add them here.
-const daySpanOptions = {
-  7: 7,
-  14: 14,
-}
-
-// 2H
-// daySpanOptionsArr is something that I find useful. This converts our daySpanOptions object into 
-// an array of the values for each property. If our object was
-// {
-//   7: 'bar',
-//   14: 14,
-// }
-// then this array would be ['bar', 14];
-// we can also use Object.keys() to do the same but use the keys from each property instead.
-// the reason I like doing this is because now instead of having to write out each individual button
-// we can just map over the options. Makes it really quick and simple if any updates are needed.
-const daySpanOptionsArr = Object.values(daySpanOptions);
-
-// _________ORIGINAL BELOW_________
-
-// import "./ForcastBox.css";
-// import { getWeatherIcon, getWeatherLabel } from "../getWeatherStatus.jsx";
-// import { TbTemperatureFahrenheit } from "react-icons/tb";
-// import { useState, useRef } from "react";
-// import { FormattedDate } from "../FormattedDate.jsx";
-// import { LoadingIcon } from "../LoadingIcon.jsx";
-
-// export const ForcastBox = ({
-//   WeatherIcon,
-//   maxTemp,
-//   minTemp,
-//   forcastDate,
-//   isLoading,
-// }) => {
-//   const scrollContainerRef = useRef(null);
-//   const [selectedDay, setSelectedDay] = useState(7);
-//   const [buttonParam, setButtonParam] = useState(7);
-
-//   const handleSelectDay = (param) => {
-//     const paramSelect = Number(param);
-//     setSelectedDay(paramSelect);
-//     setButtonParam(paramSelect === 7 ? 7 : 14);
-//     if (scrollContainerRef.current) {
-//       scrollContainerRef.current.scrollTop = 0;
-//     }
-//   };
-
-//   const day7Class = selectedDay === 7 ? "clicked" : "";
-//   const day14Class = selectedDay === 14 ? "clicked" : "";
-//   const forecastDaysClass = selectedDay === 7 ? "hide-scrollbar" : "";
-
-//   const isValidForecast = forcastDate?.length;
-
-//   return (
-//     <div id="forcast-box-contents-container">
-//       <div id="contents-gap">
-//         <div id="forcast-text-and-day-selector-container">
-//           <p id="forcast-text">Forecast</p>
-//           {isValidForecast && (
-//             <div id="day-selector-container">
-//               <button
-//                 id="day-param-select"
-//                 className={day7Class}
-//                 onClick={() => handleSelectDay(7)}
-//               >
-//                 <p>7 Days</p>
-//               </button>
-//               <button
-//                 id="day-param-select"
-//                 className={day14Class}
-//                 onClick={() => handleSelectDay(14)}
-//               >
-//                 <p>14 Days</p>
-//               </button>
-//             </div>
-//           )}
-//         </div>
-//         <div
-//           id="forcast-days-container"
-//           className={forecastDaysClass}
-//           ref={scrollContainerRef}
-//         >
-//           <div id="clickable-days-container">
-//             {isLoading ? (
-//               <div id="loading-icon-forcast-box">
-//                 <LoadingIcon />
-//               </div>
-//             ) : isValidForecast ? (
-//               forcastDate
-//                 .slice(0, buttonParam)
-//                 .map((eachIndividualDate, dateID) => (
-//                   <div key={dateID} id="clickable-forcast-day">
-//                     <div id="icon-and-high-low-container">
-//                       <div id="weather-icon-styling">
-//                         {getWeatherIcon(getWeatherLabel(WeatherIcon[dateID]))}
-//                       </div>
-//                       <div id="temp-high-low-and-f-symbol">
-//                         <p id="temp-high-low-styling">
-//                           {Math.round(minTemp[dateID])} /{" "}
-//                           {Math.round(maxTemp[dateID])}
-//                         </p>
-//                         <div id="fahrenheit-symbol">
-//                           <TbTemperatureFahrenheit />
-//                         </div>
-//                       </div>
-//                     </div>
-//                     <div id="year-month-day-container">
-//                       <FormattedDate acceptedDate={eachIndividualDate} />
-//                     </div>
-//                   </div>
-//                 ))
-//             ) : (
-//               <p id="error-message-forcast-box">No Data Available</p>
-//             )}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
