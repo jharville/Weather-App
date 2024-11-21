dfb2945d0fead4181183da889579154ce4a532a6
jharville <153254589+jharville@users.noreply.github.com>
2024-11-20 19:02:58 -0600

    Fixed some stuff in current box

--- src/Screens/ResultsPage/CurrentWeatherBox.jsx
+++ src/Screens/ResultsPage/CurrentWeatherBox.jsx
@@ -1,89 +1,119 @@
+import React, { useState, useEffect } from "react";
 import "./CurrentWeatherBox.css";
 import { getWeatherIcon } from "../getWeatherStatus.jsx";
 import { WiHumidity } from "react-icons/wi";
-import { FaWind } from "react-icons/fa";
-import { SiRainmeter } from "react-icons/si";
-import { CurrentTime } from "./CurrentTime";
+import ReactAnimatedWeather from "react-animated-weather";
+import { LoadingIcon } from "../LoadingIcon.jsx";
+// import { DaylightArc } from "./SunPositionTracker.jsx";
+import { format, parseISO, addMinutes } from "date-fns";
+import { SunPositionTracker } from "./SunPositionTracker";
 
-// "-" is given to props as a default value to ensure a fallback
+const windIcon = {
+  icon: "WIND",
+  color: "white",
+  size: 52,
+  animate: true,
+};
+
 export const CurrentWeatherBox = ({
   isLoading,
   isLoadingDone,
-  isLoadingRejected,
+  weather,
   temperature,
-  humidity = "-",
-  rain = "-",
-  windSpeed = "-",
+  humidity,
+  windSpeed,
   newCity,
   weatherFetchError,
   generalWeatherCondition,
 }) => {
-  const formattedHumidity = humidity ? humidity : null;
-  const formattedWindSpeed = windSpeed ? windSpeed : null;
-  const formattedRain = rain ? rain : null;
-  const formattedTemperature = temperature ? temperature : null;
+  // Per chat, and my experience, the time returned from open-mateo is not always updated in real time.
+  // The hour is always correct and the local times around the world are correct, but the minutes are up to 10 minutes off.
+  // This is frustrating because if you log the time via weather?.current?.time, you'll see that other variables
+  // update but time doesn't. I may need to use another package for time. I'd like your opinion here.
+
+  const [incrementedTime, setIncrementedTime] = useState(null);
+  const rawTime = weather?.current?.time;
+  useEffect(() => {
+    if (rawTime) {
+      const initialTime = parseISO(rawTime);
+      setIncrementedTime(initialTime);
+      const interval = setInterval(() => {
+        setIncrementedTime((previousTime) => addMinutes(previousTime, 1));
+      }, 60000);
+
+      return () => clearInterval(interval);
+    }
+  }, [rawTime]);
+
+  const formattedTime = incrementedTime ? format(incrementedTime, "h:mm a") : null;
+
+  const formattedHumidity = humidity || null;
+  const formattedWindSpeed = windSpeed || null;
+  const formattedTemperature = temperature || null;
   const generalWeatherIcon = isLoadingDone ? getWeatherIcon(generalWeatherCondition) : null;
 
   return weatherFetchError ? (
     <div id="error-message-current-box">{weatherFetchError}</div>
+  ) : isLoading ? (
+    <div id="loading-text-and-icon">
+      <p id="loading-text">Loading</p>
+      <LoadingIcon />
+    </div>
   ) : (
     <div id="current-weather-box-contents">
       <div id="current-weather-top-row-container">
         <p id="current-weather-in-text">Current Weather in:</p>
         <p id="current-weather-city-name">{newCity}</p>
-        <div id="current-time">
-          <CurrentTime />
-        </div>
+        <div id="current-time">{formattedTime}</div>
       </div>
       <div id="current-weather-middle-row-container">
-        {generalWeatherIcon}
+        <div id="current-icon-status-container">{generalWeatherIcon}</div>
         <div id="current-weather-temp-container">
           <div>
             <h1 id="temp">
-              {isLoading ? (
-                <p id="temp-loading-text">Loading...</p>
-              ) : (
-                <div id="temp-and-fahrenheit-box">
-                  {formattedTemperature}
-                  <p id="fahrenheit-symbol">{`\u00B0F`}</p>
-                </div>
-              )}
+              <div id="temp-and-fahrenheit-box">
+                <span id="minus-symbol">{formattedTemperature < 0 ? "-" : ""}</span>
+                {Math.abs(formattedTemperature)}
+                <p id="fahrenheit-symbol">{`\u00B0F`}</p>
+              </div>
             </h1>
+
             <p id="current-weather-condition-text">{generalWeatherCondition}</p>
           </div>
         </div>
       </div>
 
       <div id="current-weather-bottom-row-container">
-        <div id="bottom-current-box">
-          <div id="humidity-value-and-percent-container">
-            <p id="humidity-value">{formattedHumidity}</p>
-            <p id="humidity-percent-sign">%</p>
-          </div>
-          <div id="humidity-icon">
-            <WiHumidity />
-          </div>
+        <div id="bottom-element-box">
+          <SunPositionTracker
+            sunriseTime={weather?.daily?.sunrise[0]}
+            sunsetTime={weather?.daily?.sunset[0]}
+            currentTime={weather?.current?.time}
+          />
         </div>
-
-        <div id="bottom-current-box">
+        <div id="bottom-element-box">
           <div id="wind-value-mph-container">
             {formattedWindSpeed}
             <p id="wind-mph">mph</p>
           </div>
           <div id="wind-icon-message">
             <div id="wind-icon">
-              <FaWind />
+              <ReactAnimatedWeather
+                icon={windIcon.icon}
+                color={windIcon.color}
+                size={windIcon.size}
+                animate={windIcon.animate}
+              />
             </div>
           </div>
         </div>
-
-        <div id="bottom-current-box">
-          <div id="rain-value-and-percent-container">
-            {formattedRain}
-            <p id="rain-percent-sign">%</p>
+        <div id="bottom-element-box">
+          <div id="humidity-value-and-percent-container">
+            <p id="humidity-value">{formattedHumidity}</p>
+            <p id="humidity-percent-sign">%</p>
           </div>
-          <div id="rain-icon">
-            <SiRainmeter />
+          <div id="humidity-icon">
+            <WiHumidity />
           </div>
         </div>
       </div>
--- src/Screens/useWeatherFetch.js
+++ src/Screens/useWeatherFetch.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import axios from "axios";
 
 export const loadingStatuses = {
@@ -11,15 +11,15 @@ export const loadingStatuses = {
 /**
  * useWeatherFetch is a custom hook for fetching weather variables from the Open Mateo API.
  * This destructured hook returns the following values:
- * 
+ *
 weather:
 Type: Object or Null.
-Description: Contains the weather data retrieved from the API. Initially, it's null before any data is fetched. 
+Description: Contains the weather data retrieved from the API. Initially, it's null before any data is fetched.
 Once data is fetched successfully, it holds the weather data structure returned from the Open Meteo API.
 
 fetchWeather:
 Type: Function.
-Description: A function that takes a cityName as an argument and initiates the process of fetching weather data for that city. 
+Description: A function that takes a cityName as an argument and initiates the process of fetching weather data for that city.
 This function performs the API requests to retrieve geographical coordinates and weather data, handles errors, and updates the states accordingly.
 
 weatherFetchError:
@@ -47,20 +47,30 @@ export const useWeatherFetch = () => {
         isRejected = false;
         setLoadingStatus(loadingStatuses.loading);
         const geographicalCoordinatesResponse = await axios.get(
-          `https://nominatim.openstreetmap.org/search?q=${cityName}&format=json&limit=1`
+          `https://nominatim.openstreetmap.org/search?q=${cityName}&format=json&limit=1`,
+          {
+            headers: {
+              "Cache-Control": "no-cache, no-store, must-revalidate",
+              Pragma: "no-cache",
+              Expires: "0",
+            },
+          }
         );
         if (geographicalCoordinatesResponse.data.length === 0) {
+          isRejected = true;
           setWeatherFetchError("City not found. Please check the city name.");
+          setLoadingStatus(loadingStatuses.rejected);
+          return;
         }
         const { lat, lon } = geographicalCoordinatesResponse.data[0];
         const weatherResponse = await axios.get(
-          `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weather_code,pressure_msl,surface_pressure,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,visibility,evapotranspiration,et0_fao_evapotranspiration,vapour_pressure_deficit,wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_120m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_120m,temperature_180m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=14&timezone=auto`
+          `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weather_code,pressure_msl,surface_pressure,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,visibility,evapotranspiration,et0_fao_evapotranspiration,vapour_pressure_deficit,wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_120m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_120m,temperature_180m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=14&timezone=auto&timestamp=${Date.now()}`
         );
-
         setWeather(weatherResponse.data);
       } catch (Error1) {
         setWeather(null);
         setLoadingStatus(loadingStatuses.rejected);
+        setWeatherFetchError("Please check internet connection.");
         isRejected = true;
       } finally {
         if (!isRejected) {
